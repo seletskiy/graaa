cb81c1f6f0da03806d6fba0ff9c539542ffe62f1
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2015-02-07 16:45:03 +0300

    itemss

--- items.go
+++ items.go
@@ -1,7 +1,17 @@
 package main
 
-type Items []*Item
+import (
+	"strconv"
 
+	"github.com/seletskiy/go-android-rpc/android"
+	"github.com/seletskiy/go-android-rpc/android/sdk"
+)
+
+var (
+	viewId int = 14
+)
+
+type Items []*Item
 type Item struct {
 	Content string
 }
@@ -10,10 +20,22 @@ func NewItems() *Items {
 	return &Items{}
 }
 
-func NewItem() *Item {
-	return &Item{}
+func NewItem(content string) *Item {
+	return &Item{Content: content}
 }
 
-func showItems(items *Items) {
+func (item Item) Display() {
+	viewId := viewId + 1
 
+	textView := android.CreateView(
+		strconv.Itoa(viewId), "android.widget.TextView").(sdk.TextView)
+	textView.SetText1s(item.Content)
+
+	android.AttachView(textView, mainLayoutId)
+}
+
+func (items Items) Display() {
+	for _, item := range items {
+		item.Display()
+	}
 }
--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"github.com/seletskiy/go-android-rpc/android"
+	"strconv"
 
 	// required for linking
 	_ "github.com/seletskiy/go-android-rpc/android/modules"
 )
 
+var mainLayoutId string
+
 func start() {
+	layoutResponse := android.GetLayoutById("main_layout")
+	mainLayoutId = layoutResponse["layout_id"].(string)
+
 	items := NewItems()
-	item := NewItem()
-	*items = append(*items, item)
-	showItems(items)
+	for i := 0; i < 10; i++ {
+		item := NewItem("aaa " + strconv.Itoa(i))
+		*items = append(*items, item)
+	}
+
+	items.Display()
 }
 
 func main() {
